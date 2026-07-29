609e15857da321743f47a06f5934cc287d849c24
Afia Nasumudeen <147170685+afianas@users.noreply.github.com>
2026-07-29 22:40:25 +0530

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.py
@@ -0,0 +1,25 @@
+from collections import Counter
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand)%groupSize!=0:
+            return False
+
+        count=Counter(hand)
+        
+        for num in sorted(count):
+            freq=count[num]
+
+            if freq==0:
+                continue
+            for numNext in range(num,num+groupSize):
+                if count[numNext]<freq:
+                    return False
+                count[numNext]-=freq
+        return True
+
+
+        
+                
+            
+                
+
